cf59673c9e52ce4eda1746876c08b57e322f1769
jlchereau <jlchereau@msn.com>
2024-04-09 19:56:46 +0200

    Added more unit tests

---
+++ tests/tools/aws_test.py
@@ -0,0 +1,8 @@
+from testbook import testbook
+
+@testbook('./tools/aws.ipynb', execute=True)
+def test_notebook(nb):
+   nb_completion = nb.get('nb_completion')
+   ret = nb_completion()
+   assert isinstance(ret, int)
+   assert ret > 0 and ret <= 10
