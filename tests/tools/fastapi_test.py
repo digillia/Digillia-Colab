2aaf400e3fbf803ca182c8185ea5f5a4f701d0dc
jlchereau <jlchereau@msn.com>
2024-04-09 21:38:41 +0200

    Changed test framework from testbook to nbmake

--- tests/tools/fastapi_test.py
+++
@@ -1,8 +0,0 @@
-from testbook import testbook
-
-@testbook('./tools/fastapi.ipynb', execute=True)
-def test_notebook(nb):
-   nb_completion = nb.get('nb_completion')
-   ret = nb_completion()
-   assert isinstance(ret, int)
-   assert ret > 0 and ret <= 10
